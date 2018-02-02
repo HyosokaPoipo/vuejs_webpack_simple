ea000efc65d28f11742be3b9f552a417f185de59
Maryadi <maryadi@astrnt.co>
2018-02-02 11:20:59 +0700

    add boruto style

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="./assets/boruto_kage.png">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+      <li><a href="https://www.youtube.com/channel/UCXu-D19AiIJZptbg8Z549xg" target="_blank">Youtube</a></li>
+      <li><a href="http://dimensitutupbotol.blogspot.co.id/" target="_blank">Blog</a></li>
+      <li><a href="https://www.facebook.com/Hyosoka.Poipo" target="_blank">Facebook</a></li>
+      <li><a href="https://twitter.com/hyosokapoipo" target="_blank">Twitter</a></li>
+      <li><a href="https://github.com/HyosokaPoipo/vuejs_webpack_simple" target="_blank">Github</a></li>
     </ul>
-    <h2>Ecosystem</h2>
+    <h2>Popular Videos</h2>
     <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      <li><a href="https://www.youtube.com/watch?v=2KYNQmszbJ8" target="_blank">Meet Grandpa</a></li>
+      <li><a href="https://www.youtube.com/watch?v=70IcY0YZMqI" target="_blank">Visit Ichiraku Ramen</a></li>
+      <li><a href="https://www.youtube.com/watch?v=Jbi54W6WMeM" target="_blank">Himawari's Byakugan</a></li>
+      <li><a href="https://www.youtube.com/watch?v=I1OTkS3KZK0" target="_blank">Naruto's Prank</a></li>
     </ul>
   </div>
 </template>
@@ -24,7 +25,7 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Boruto Lovers Page hehe :D'
     }
   }
 }
